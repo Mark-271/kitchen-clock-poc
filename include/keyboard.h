fb3af45842998d2a162999a2035c5d3c244f53f9
Mark Sungurov <mark.sungurov@gmail.com>
2021-01-30 23:41:43 +0200

    keyboard: Rework struct kbd
    Signed-off-by: Mark Sungurov <mark.sungurov@gmail.com>

--- include/keyboard.h
+++ include/keyboard.h
@@ -4,19 +4,22 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-struct kbd {
+
+typedef void (*kbd_btn_event_t)(int button, bool pressed);
+
+struct kbd_gpio {
 	uint32_t port;
-	uint16_t l1;	/* Sampling line L1 */
-	uint16_t l2;	/* Sampling line L2 */
-	uint16_t r1;	/* Scan line R1 */
-	uint16_t r2;	/* Scan line R2 */
+	uint16_t l1;		/* sampling line L1 */
+	uint16_t l2;		/* sampling line L2 */
+	uint16_t r1;		/* scan line R1 */
+	uint16_t r2;		/* scan line R2 */
 };
 
-enum kbd_btn {
-	KBD_BTN_1 = 1,
-	KBD_BTN_2,
-	KBD_BTN_3,
-	KBD_BTN_4
+struct kbd {
+	struct kbd_gpio gpio;	/* user data */
+	kbd_btn_event_t cb;	/* callback */
+	uint16_t lookup[5];	/* mapping: btn --> GPIO port value */
+	int btn;		/* button code */
 };
 
 typedef void (*kbd_btn_event_t)(enum kbd_btn btn, bool pressed);

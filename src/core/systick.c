ce83729683bfbc3c08b54291ea2bf4759c5dd336
Mark Sungurov <mark.sungurov@gmail.com>
2021-03-05 21:24:39 +0200

    core: systick: Define sys_tick_handler
    Signed-off-by: Mark Sungurov <mark.sungurov@gmail.com>

--- src/core/systick.c
+++ src/core/systick.c
@@ -1,9 +1,27 @@
 #include <core/systick.h>
 #include <libopencm3/cm3/systick.h>
+#include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/rcc.h>
 
 #define SYSTICK_FREQ		0x3e8 /* 1000 overflows per second */
 #define SYSTICK_CNT_MASK	0xffffff
+
+static volatile uint32_t ticks;
+
+/**
+ * Systick handler.
+ *
+ * Since systick handler is an entry of vector table, it is also copied
+ * to RAM during vector table relocation. However there is no possibility
+ * to request systick by its irq number when interacting with irq manager
+ * for the reason that systick is implemented inside Cortex-M3 core, and is
+ * considered primarely as exception, not an interrupt. As systick handler
+ * is defined as a weak symbol, caller should redefine it.
+ */
+void sys_tick_handler(void)
+{
+	ticks++;
+}
 
 /**
  * Initialize systick timer.

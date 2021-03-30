7195413193b52f26d44ac5b0cc2836090d8a5df0
Mark Sungurov <mark.sungurov@gmail.com>
2021-03-30 21:36:13 +0300

    drivers: rtc: Add function rtc_read_time
    Signed-off-by: Mark Sungurov <mark.sungurov@gmail.com>

--- src/drivers/rtc.c
+++ src/drivers/rtc.c
@@ -8,7 +8,7 @@
 #define RTC_CR			0x0e
 #define RTC_SR			0x0f
 #define RTC_SECONDS		0x00	/* DS3231 seconds register address */
-#define RTC_TM_BUF_LEN		7	/* Numner of date/time registers */
+#define RTC_TM_BUF_LEN		4	/* Number of time registers */
 
 static uint8_t dec2bcd(uint8_t val)
 {
@@ -18,6 +18,34 @@ static uint8_t dec2bcd(uint8_t val)
 static uint8_t bcd2dec(uint8_t val)
 {
 	return (val & 0x0f) + (val >> 4) * 10;
+}
+
+/**
+ * Read time registers from RTC device.
+ *
+ * Read values contained in time specific DS3231 registers:
+ * seconds, minutes, hours, day of the week.
+ *
+ * @param obj RTC device
+ * @return 0 on success or negative value on error
+ */
+int rtc_read_time(struct rtc *obj)
+{
+	int ret;
+	size_t i;
+	uint8_t temp[RTC_TM_BUF_LEN];
+	uint8_t buf[RTC_TM_BUF_LEN];
+
+	ret = i2c_read_buf_poll(obj->addr, RTC_SECONDS, temp, RTC_TM_BUF_LEN);
+	if (ret != 0)
+		return ret;
+
+	for (i = 0; i <  RTC_TM_BUF_LEN; i++)
+		buf[i] = bcd2dec(temp[i]);
+
+	memcpy(&obj->tm, &buf[0], RTC_TM_BUF_LEN);
+
+	return 0;
 }
 
 /**
